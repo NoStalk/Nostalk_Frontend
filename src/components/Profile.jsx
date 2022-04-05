5f9a266039461da23d2d8f228a85e8cc11c83d2d
zireal23 <skasif232000@gmail.com>
2022-04-05 12:44:44 +0530

    Minor changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
 import '../App.css'
+import {
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  StatArrow,
+  StatGroup,
+} from '@chakra-ui/react';
 
 const Profile = () => {
   return (
     <div className="profile">
       <div className='infotable'>
+          <StatGroup>
+              <Stat>
+                <StatLabel>Sent</StatLabel>
+                <StatNumber>345,670</StatNumber>
+                <StatHelpText>
+                 <StatArrow type='increase' />
+                      23.36%
+                </StatHelpText>
+              </Stat>
 
+              <Stat>
+                <StatLabel>Clicked</StatLabel>
+                <StatNumber>45</StatNumber>
+                <StatHelpText>
+                  <StatArrow type='decrease' />
+                  9.05%
+               </StatHelpText>
+             </Stat>
+            </StatGroup>
       </div>
       <div className='information'>
          <div className='submissionpiechart'>
