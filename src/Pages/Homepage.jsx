0e73a58f35516fe0691511bf146dba6fbd9321dd
zireal23 <skasif232000@gmail.com>
2022-06-08 14:19:41 +0530

    Finished the landing page

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 import Navdiv from "../components/Navdiv";
 import { Text } from "@chakra-ui/react";
@@ -13,23 +13,78 @@ const Homepage = (props) => {
   const { ref, inView } = useInView({
     threshold: 0.2,
   });
+
   const animation = useAnimation();
   useEffect(() => {
+    console.log(inView);
     if (inView)
       animation.start({
-        x: 0,
+        opacity: 1,
         transition: {
-          type: 'spring',
-          duration: 1,
-        }
-      })
+          type: "spring",
+          duration: 2,
+        },
+      });
     else {
       animation.start({
-        x: '-100vw',
-      })
+        opacity: 0,
+      });
     }
   }, [inView]);
-  
+
+  const { ref: ref2, inView: inView2 = inView } = useInView({
+    threshold: 0.2,
+  });
+  const animationSecond = useAnimation();
+  useEffect(() => {
+    if (inView2)
+      animationSecond.start({
+        scale: 1,
+        transition: {
+          duration: 1,
+        },
+      });
+    else
+      animationSecond.start({
+        scale: 0.5,
+      });
+  }, [inView2]);
+
+  const { ref: ref3, inView: inView3 } = useInView({
+    threshold: 0.2,
+  });
+  const animationThird = useAnimation();
+  useEffect(() => {
+    if (inView3)
+      animationThird.start({
+        y: 0,
+        transition: {
+          duration: 0.5,
+        },
+      });
+    else
+      animationThird.start({
+        y: "-400vh",
+      });
+  }, [inView3]);
+
+    const { ref: ref4, inView: inView4 } = useInView({
+      threshold: 0.2,
+    });
+    const animationFourth = useAnimation();
+    useEffect(() => {
+      if (inView4)
+        animationFourth.start({
+          x: 0,
+          transition: {
+            duration: 0.5,
+          },
+        });
+      else
+        animationFourth.start({
+          x: "-400vw",
+        });
+    }, [inView4]);
 
   return (
     <div className="homepage ">
@@ -77,7 +132,6 @@ const Homepage = (props) => {
           </motion.div>
         </div>
       </div>
-
       <img
         src="./background/waves.svg"
         alt="wave"
@@ -87,13 +141,8 @@ const Homepage = (props) => {
           border: "transparent",
         }}
       />
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-        className="homepage-second"
-      >
-        <div className="homepage-second-text">
+      <motion.div ref={ref2} className="homepage-second">
+        <motion.div className="homepage-second-text">
           <Title style={{ marginBottom: "10%" }}>
             Statistics of every question on every website that you have ever
             solved.
@@ -103,16 +152,73 @@ const Homepage = (props) => {
             codechef, atcoder.
           </Title>
           <Title>Analyse your interview preparation from Leetcode.</Title>
-        </div>
+        </motion.div>
 
-        <img
+        <motion.img
+          animate={animationSecond}
           src="./illustrations/Data.svg"
           alt="data"
           style={{ opacity: "0.9", width: "50%", height: "50%" }}
         />
       </motion.div>
-      <div className="homepage-third"></div>
-      <div className="homepage-fourth"></div>
+      <img
+        src="./background/divider23.svg"
+        alt="divider2-3"
+        style={{
+          display: "flex",
+          backgroundColor: "#090a0a",
+          border: "transparent",
+        }}
+      />
+      <div ref={ref3} className="homepage-third">
+        <div className="homepage-third-text" style={{ marginRight: "7%" }}>
+          <Title>
+            Stay Upto Date with all the coding contests as well as hiring
+            challenges.
+          </Title>
+        </div>
+        <motion.img
+          animate={animationThird}
+          src="./illustrations/calendar.svg"
+          alt="contests"
+          style={{
+            opacity: "0.9",
+            width: "50%",
+            height: "50%",
+            marginLeft: "2%",
+          }}
+        />
+      </div>
+
+      <img
+        src="./illustrations/divider34.svg"
+        alt="divider2-3"
+        style={{
+          display: "flex",
+          backgroundColor: "#11002c",
+          border: "transparent",
+        }}
+      />
+
+      <div className="homepage-fourth" ref={ref4}>
+        <div className="homepage-fourth-text" style={{ marginRight: "7%" }}>
+          <Title>
+            Compete with your friends and track your progress using the NOSTALK
+            leaderboards.
+          </Title>
+        </div>
+        <motion.img
+          animate={animationFourth}
+          src="./illustrations/rankings.svg"
+          alt="contests"
+          style={{
+            opacity: "0.9",
+            width: "50%",
+            height: "50%",
+            marginLeft: "2%",
+          }}
+        />
+      </div>
 
       <div className="homepage-fifth"></div>
     </div>
