085527cd72a94e4bab54807f021266b2e7034221
Ronak Sanpui <ronaksanpui@gmail.com>
2022-07-24 22:41:41 +0530

    fixed reload bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,24 @@
-import { useEffect } from 'react'
 import "./App.css";
 import SideBar from './Pages/SideBar';
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AccountInfo, Contests, Friends, Homepage, Leaderboard, Profile, Submissions, Login } from "./Pages";
 
-import { userData } from './features/userDataSlice'
 import AuthRequired from './components/AuthRequired';
 import useAuth from './hooks/useAuth';
-import axios from 'axios';
 
 
 function App() {
 
-  const { isLoggedIn, logIn } = useAuth();
-
-  const location: any = useLocation();
-  const from: string = location.state?.from?.pathname;
-
-  useEffect(() => {
-    if (!process.env.REACT_APP_BACKEND_URL) {
-      console.error(new Error('REACT_APP_BACKEND_URL enviroment variable not set'))
-      return;
-    }
-    if (isLoggedIn) {
-      return;
-    }
-    try {
-
-      axios.get<userData>(process.env.REACT_APP_BACKEND_URL + '/refresh', { withCredentials: true })
-        .then(response => {
-          console.log(from);
-          logIn(response.data, from);
-        })
-    }
-    catch (error) {
-      console.error(error);
-    }
-  }, [])
+  const user = useAuth();
 
   return (
     <div className="App" >
-      {isLoggedIn && <SideBar />}
+      {user.isLoggedIn && <SideBar />}
 
       <Routes>
-        <Route path="/" element={< Homepage isLoggedIn={isLoggedIn} />} />
-        <Route path="/login" element={< Login />} />
         <Route element={<AuthRequired />}>
+          <Route path="/" element={< Homepage isLoggedIn={user.isLoggedIn} />} />
+          <Route path="/login" element={< Login />} />
           <Route path="/accountinfo" element={< AccountInfo />} />
           <Route path="/contests" element={< Contests />} />
           <Route path="/friends" element={< Friends />} />
